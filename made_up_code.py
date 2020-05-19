f60dee8d1e71808ca045c3f2f37f78f9508ca663
mperry-uni <30280370+mperry-uni@users.noreply.github.com>
2020-05-19 19:54:42 +1000

    Add made up code

---
+++ made_up_code.py
@@ -0,0 +1,30 @@
+i=0
+while i<50:
+    for i in range(5):
+        while False:
+            print('blah')
+            for j in range(len([1,2,3])):
+                while 'foo':
+                    if 'bar':
+                        if not 'blah':
+                            def bar(blah):
+                                def foo(qwerty):
+                                    print('bar')
+    i += 1
+                     
+                     
+# xhjhkuytrezsdfghvjkbuytrzfghjkbhugfyd
+
+i=0
+while i<50:
+    for i in range(5):
+        while False:
+            print('blah')
+            for j in range(len([1,2,3])):
+                while 'foo':
+                    if 'bar':
+                        if not 'blah':
+                            def bar(blah):
+                                def foo(qwerty):
+                                    print('bar')
+    i += 1
